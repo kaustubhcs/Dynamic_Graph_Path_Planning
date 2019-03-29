90a98b37bb1d692daac9ff1a627672b0fb9b06e7
Kaustubh Shivdikar <kaustubhcs07@gmail.com>
2019-03-28 20:23:58 -0400

    Synthetic Data Generator Created

---
+++ Spark-Pagerank/datagen/src/code/main.cpp
@@ -0,0 +1,30 @@
+// Created by Kaustubh Shivdikar
+//
+//  (C) All Rights Reserved
+
+
+# include "../lib/functions.h"
+# include "../lib/debugger.h"
+
+using namespace std;
+
+// Global Variables
+
+
+
+
+
+// ************ MAIN FUNCTION ************
+
+
+int main (int argc, char *argv[]) {
+
+		Input input;
+		get_inputs (argc, argv, input, 1);
+
+
+
+		cout << "\n\e[1;31mProgram End\e[0m\n\n\n";
+		return 0;
+}
+
